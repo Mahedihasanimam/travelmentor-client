d7c5ddbfa5e224cfa4235b2dd59603d3f78a4c20
mahedi066 <imam25349@gmail.com>
2024-04-27 23:29:44 +0600

    featured collection added

---
+++ src/components/Gellary.jsx
@@ -0,0 +1,24 @@
+
+
+const Gellary = () => {
+    return (
+        <div>
+            <section className="py-6 dark:bg-gray-100 dark:text-gray-900">
+	<div className="container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4">
+		<img src="https://images.pexels.com/photos/1371360/pexels-photo-1371360.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/1271619/pexels-photo-1271619.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/307008/pexels-photo-307008.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/38238/maldives-ile-beach-sun-38238.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/2104742/pexels-photo-2104742.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/2169857/pexels-photo-2169857.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/1659438/pexels-photo-1659438.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/1268855/pexels-photo-1268855.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img alt="" className="w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" src="https://images.pexels.com/photos/279574/pexels-photo-279574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+		<img  src="https://miro.medium.com/v2/resize:fit:828/format:webp/1*OVzL5R-414Dr5sS6Oeayjw.jpeg" alt="" className="w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square hover:scale-105 transition-all cursor-pointer" />
+	</div>
+</section>
+        </div>
+    );
+};
+
+export default Gellary;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { useLoaderData } from "react-router-dom";
 import SwiperSlider from "../components/SwiperSlider";
 import TouristCard from "../components/TouristCard";
+import Gellary from "../components/Gellary";
 
 
 
@@ -14,11 +15,16 @@ const Home = () => {
            <SwiperSlider></SwiperSlider>
            <div>
            <h1 className="text-4xl font-bold text-center mt-16 mb-8 border-b-4 pb-2  w-fit border-solid border-green-500">Tourists Spots</h1>
+           
             <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-4">
             {
                 tourist.slice(0,6).map(item=><TouristCard item={item} key={item._id}></TouristCard>)
             }
             </div>
+         <div>
+         <h1 className="text-4xl font-bold text-center mt-20 mb-4 border-b-4 pb-2  w-fit border-solid border-green-500">Featured Collection</h1>
+         <Gellary></Gellary>
+         </div>
         </div>
         </div>
     );
